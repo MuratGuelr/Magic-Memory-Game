d68097422a48b75547d396c906bf8b78b7f6e056
MuratGuelr <107948385+MuratGuelr@users.noreply.github.com>
2023-01-26 14:09:07 +0000

    cards updated.

--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import './SingleCard.css'
 
-export default function SingleCard({cards}) {
+export default function SingleCard({card ,handleChoise, flipped, disabled}) {
+
+const handleClick = () => {
+  if(!disabled){
+    handleChoise(card)
+    
+  }
+}
+
   return (
-        <div className="card-grid">
-        {cards.map(card => (
+       
           <div className='card' key={card.id}>
-            <div>
-              <img classname="front" src={card.src} alt="card front"/>
-              <img className='back' src="/img/cover.png" alt="card back" />
+            <div className={flipped ? "flipped" : ""}>
+              <img 
+              className="front" 
+              src={card.src} 
+              alt="card front"
+              />
+              
+              <img 
+              className='back' 
+              src="/img/cover.png" 
+              alt="card back" 
+              onClick={handleClick} 
+              />
+
             </div>
           </div>
-        ))}
-      </div>
+
   )
 }
+
